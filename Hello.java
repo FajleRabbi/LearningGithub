e5e0ff65a9488fee45fb24a552f0093bd20c2db1
fajlerabbi <fajlerabbi824@gmail.com>
2019-01-28 11:42:34 +0600

    Adeed a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[] args){
         System.out.printIn("Vs code is awesome...");
+        greeting("FajleRabbi");
+    }
+    public static void display(String name){
+        System.out.printIn("welcome" + name);
     }
 }
